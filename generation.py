82c7d3dafb509c14666155b81f2d584dbace3a1f
Sahil Verma <v.sahil1@gmail.com>
2023-06-06 04:55:39 -0400

    corrected generation.py

--- generation.py
+++ generation.py
@@ -6,13 +6,60 @@ from typing import List
 import torch
 
 # from tokenizer import Tokenizer
-from model2 import Transformer
+# from model2 import Transformer
+from model import Transformer
 
 
 class LLaMA:
     def __init__(self, model: Transformer, tokenizer):
         self.model = model
         self.tokenizer = tokenizer
+
+
+    def generate_one_prompt(
+        self,
+        # prompts: List[str],
+        prompt_tokens,
+        max_gen_len: int,
+        temperature: float = 0,
+        top_p: float = 0.95,
+    ) -> List[str]:
+
+        assert prompt_tokens.shape[0] == 1      ## only generating one prompt at a time
+
+        # import ipdb; ipdb.set_trace()
+        for _ in range(max_gen_len):
+            ## generate one token at a time
+            logits = self.model.forward(prompt_tokens, 0, train=False)
+            # print("logits", logits.shape)
+            if temperature > 0:
+                probs = torch.softmax(logits / temperature, dim=-1)
+                next_token = sample_top_p(probs, top_p)
+            else:
+                next_token = torch.argmax(logits, dim=-1)
+            next_token = next_token.reshape(-1)
+            prompt_tokens = torch.cat((prompt_tokens, next_token.unsqueeze(0)), dim=1)
+            # print("prompt_tokens", prompt_tokens.shape)
+            if next_token == self.tokenizer.sep_token_id:
+                break
+
+        return self.tokenizer.decode(prompt_tokens.squeeze(0).tolist())
+        
+        # decoded = []
+        # for i, t in enumerate(prompt_tokens.tolist()):
+        #     # cut to max gen len
+        #     # t = t[: len(prompt_tokens[i]) + max_gen_len]
+        #     # cut to eos tok if any
+        #     try:
+        #         t = t[: t.index(self.tokenizer._eos_token)]
+        #     except ValueError:
+        #         pass
+        #     decoded = self.tokenizer.decode(t)
+        # return decoded
+
+        # return prompt_tokens
+    
+
 
     def generate(
         self,
@@ -22,6 +69,8 @@ class LLaMA:
         temperature: float = 0.8,
         top_p: float = 0.95,
     ) -> List[str]:
+
+        # import ipdb; ipdb.set_trace()
         bsz = len(prompt_tokens)
         params = self.model.params
         assert bsz <= params.max_batch_size, (bsz, params.max_batch_size)
@@ -35,14 +84,15 @@ class LLaMA:
 
         tokens = torch.full((bsz, total_len), self.tokenizer._pad_token_type_id).cuda().long()
         for k, t in enumerate(prompt_tokens):
-            tokens[k, : len(t)] = torch.tensor(t).long()
+            tokens[k, :len(t)] = torch.tensor(t).long()
         input_text_mask = tokens != self.tokenizer._pad_token_type_id
         start_pos = min_prompt_size
         prev_pos = 0
-        
+
         # print("token device", tokens.device)
         for cur_pos in range(start_pos, total_len):
-            logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos, train=False)
+            # logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos, train=False)     ## starting the second loop, this is only conditioning on the last token and not the entire sequence -- that is a problem. 
+            logits = self.model.forward(tokens[:, :cur_pos], prev_pos, train=False)
             # print("logits", logits.shape)
 
             if temperature > 0:
